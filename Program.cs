0d1d15ccd12235afda49fe7e52dbd5ea0c477c4e
ovod4572 <intorgsv@gmail.com>
2022-11-25 08:59:30 +0300

    Added a programm code...

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿//string[] array =  {"hello", "2", "world", ":-)"};
+string[] array = {"1234", "1567", "-2", "computer science"};
+//string[] array = {"Russia", "Denmark", "Kazan"};
+int size = array.Length;
+string[] newarray = new string[size];
+int length = 3;
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length <= length)
+    {
+        newarray[i] = array[i];
+    }
+}
+Console.WriteLine();
+PrintArray(newarray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
